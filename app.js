2ee14da306ff59a4613804c4cf6bd251a9620555
Ivanrizkys <71715895+Ivanrizkys@users.noreply.github.com>
2021-12-30 14:32:36 +0700

    Add: adding port in production

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const authrouter = require("./src/auth/router");
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // * cors
 app.use(cors());
