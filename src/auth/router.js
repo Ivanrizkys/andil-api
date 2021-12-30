f632538b2196cdb71c35cd174c839915d37adcb0
Ivanrizkys <71715895+Ivanrizkys@users.noreply.github.com>
2021-12-30 12:56:01 +0700

    Modifiy: change url endpoint signIn and signUp

--- src/auth/router.js
+++ src/auth/router.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { signup, signin } = require("./controller");
 
 const router = express.Router();
-router.post("/signup", signup);
-router.post("/signin", signin);
+router.post("/auth/signup", signup);
+router.post("/auth/signin", signin);
 
 module.exports = router;
