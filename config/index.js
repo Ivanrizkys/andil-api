1b8a457af2bbb7f2f8b71b561edda65c5b05b69d
Ivanrizkys <71715895+Ivanrizkys@users.noreply.github.com>
2021-12-31 16:45:20 +0700

    Add: load new configuration for conect database

--- config/index.js
+++ config/index.js
@@ -1,7 +1,11 @@
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 dotenv.config();
 
 module.exports = {
-    testing: process.env.test,
-    jwtSecret: process.env.JWT_SECRET
-}
+	testing: process.env.test,
+	jwtSecret: process.env.JWT_SECRET,
+	dbHost: process.env.DB_HOST,
+	dbUser: process.env.DB_USER,
+	dbPassword: process.env.DB_PASSWORD,
+	dbDatabase: process.env.DB_DATABASE,
+};
