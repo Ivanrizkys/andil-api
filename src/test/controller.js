a65f0cd873d9cfd9f0b340a65115948d3f1e1c6c
Ivanrizkys <71715895+Ivanrizkys@users.noreply.github.com>
2021-12-31 16:48:28 +0700

    Add: select table for testing in development

--- src/test/controller.js
+++ src/test/controller.js
@@ -1,7 +1,15 @@
+const db = require("../../libs/db");
+
 module.exports = {
-    test: (req, res) => {
-        res.status(200).json({
-            message: "App running succesfully"
-        })
-    }
-}
+	test: (req, res) => {
+		// * test table akun
+		const query = "SELECT * FROM Akun";
+		db.query(query, (err, result) => {
+			if (err) return res.status(400).send(err.message);
+			res.status(200).json({
+				message: "App running succesfully",
+				data: result,
+			});
+		});
+	},
+};
